d00a1fb4f16fe018fb03356a8c21d7a8dec8fb73
Vasilievkosta <vasilievkosta@gmail.com>
2025-11-19 19:51:16 +0200

    chore: added project Online Workshop

--- src/assets/data/data-projects.js
+++ src/assets/data/data-projects.js
@@ -1,23 +1,29 @@
-import imageSocial from '../../assets/images/social-project.jpg';
-import imageTodo from '../../assets/images/todo-project.jpg';
-
+import imageSocial from "../../assets/images/social-project.jpg"
+import imageTodo from "../../assets/images/todo-project.jpg"
+import imageClock from "../../assets/images/clock-project.jpg"
 
 export const arrProjects = [
-    {
-        id: 1,
-        theme: 'Sotial Network',
-        image: imageSocial,
-		href: 'http://kosta.zzz.com.ua/cv/front/',
-        description: 'The project is a social network developed using React and Redux libraries (Thunk, Axios, Saga, graphics).'
-    },
-    {
-        id: 2,
-        theme: 'Todo List',
-        image: imageTodo,
-		href: 'https://vasiliev.netlify.app/',
-        description: 'In the todolist project, the data comes from the server. Implemented authorization with saving cards on the backend'
-    },
-
-
+  {
+    id: 1,
+    theme: "Sotial Network",
+    image: imageSocial,
+    href: "https://vasilievkosta.github.io/samurai-way/",
+    description:
+      "The project is a social network developed using React and Redux libraries (Thunk, Axios, Saga, graphics).",
+  },
+  {
+    id: 2,
+    theme: "Organizer",
+    image: imageTodo,
+    href: "https://vasiliev.netlify.app/",
+    description:
+      "In the organizer project, the data comes from the server. Implemented authorization with saving cards on the backend.",
+  },
+  {
+    id: 3,
+    theme: "Online Workshop",
+    image: imageClock,
+    href: "https://clockwise-app.netlify.app/",
+    description: "The application is a service platform with server-side logic and a database.",
+  },
 ]
-
